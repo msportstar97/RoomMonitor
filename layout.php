3965751ae88510fcb13cfa531735140e926df006
Michelle Giang <msportstar97@gmail.com>
2017-07-19 14:05:36 +0900

    A website used to store room data (temperature, lights, number of people) in POSTECH from the Artik Cloud database.

---
+++ layout.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <meta http-equiv="content-type" content="text/html; charset="UTF-8">
+      <title><?php echo $title; ?></title>
+      <link rel="stylesheet" type="text/css" href="Styles/Stylesheet.css"/>
+  </head>
+  <body>
+    <div id="wrapper">
+
+      <div id="banner">
+      </div>
+
+      <nav id="navigation">
+        <ul id="nav">
+          <li><a href="index.php">Home</a></li>
+          <li><a href="room.php">Room Data</a></li>
+      </nav>
+
+      <div id="content_area">
+        <?php echo '<span style = "font-size: 16pt">' . $content . '</span>'; ?>
+      </div>
+
+      <div id="sidebar">
+      </div>
+
+    </div>
+  </body>
+</html>
---
+++ room.php
@@ -0,0 +1,14 @@
+<?php
+$title = "Room Monitor";
+
+
+$content = "<h3> Room Data </h3>
+<p>
+include 'table.php';
+
+</p>";
+
+include 'layout.php';
+
+
+?>
